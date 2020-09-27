68c49c23fa15826bc4427b1be6ced5aaf466bce2
coolmenas <coolmenas@gmai.com>
2020-09-27 16:45:37 +0300

    comments

--- TGWTest/TGWTest.cs
+++ TGWTest/TGWTest.cs
@@ -8,6 +8,10 @@ namespace TGWTest
     {
         static void Main(string[] args)
         {
+            //Jei dar tobulinant tai tikriausiai padaryciau:
+            //gudresni failu eiles pasirinkima kad nereiketu atsidayt kodo ir perkompiliuot
+            //Parameter klaseje optiona nurodyti kokio tipo tai value.
+            //Parametro access galbut padaryti pagal pilna path rakta ("Section", "SubSection","Key")
             var configParser = new ConfigParser("Data/Base_Config.txt", "Data/Project_Config.txt");
             var parameters = configParser.LoadAllParameters();
             var printer = new Printer();
